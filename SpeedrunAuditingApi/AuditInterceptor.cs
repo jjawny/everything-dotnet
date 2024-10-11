ffab44237344c8b0884c9189b2beccd5f9129589
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 12:30:23 +1000

    save changes interceptor ready

---
+++ SpeedrunAuditingApi/AuditInterceptor.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace SpeedrunAuditingApi;
+
+public class AuditInterceptor : SaveChangesInterceptor
+{
+  public override InterceptionResult<int> SavingChanges(
+    DbContextEventData eventData,
+    InterceptionResult<int> result
+  )
+  {
+    // TODO: audit
+    return base.SavingChanges(eventData, result);
+  }
+
+  public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+    DbContextEventData eventData,
+    InterceptionResult<int> result,
+    CancellationToken cancelToken = default
+  )
+  {
+    // TODO: audit
+    return base.SavingChangesAsync(eventData, result, cancelToken);
+  }
+}
