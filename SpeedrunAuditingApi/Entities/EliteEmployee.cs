69b6892fb44f140d9d6448005a0d17f28c75503a
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 19:56:00 +1000

    resolved build error

--- SpeedrunAuditingApi/Entities/EliteEmployee.cs
+++ SpeedrunAuditingApi/Entities/EliteEmployee.cs
@@ -7,7 +7,7 @@ public class EliteEmployee
   public Guid Id { get; set; }
 
   [Required]
-  public string Name { get; set; } = !null;
+  public string Name { get; set; }
   public bool IsElite { get; set; } = true;
   public Audit Audit { get; set; } = new();
 }
