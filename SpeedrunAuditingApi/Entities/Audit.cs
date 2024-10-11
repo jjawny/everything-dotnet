9f3047c4936061dbe08028e3f3d9c8c28eccdab8
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-12 08:26:47 +1000

    always tattoo createdat by default

--- SpeedrunAuditingApi/Entities/Audit.cs
+++ SpeedrunAuditingApi/Entities/Audit.cs
@@ -4,7 +4,7 @@ public class Audit
 {
   public Guid? CreatedBy { get; set; }
   public Guid? UpdatedBy { get; set; }
-  public DateTime? CreatedAt { get; set; }
+  public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
   public DateTime? UpdatedAt { get; set; }
   public bool IsDeleted { get; set; } = false;
 }
