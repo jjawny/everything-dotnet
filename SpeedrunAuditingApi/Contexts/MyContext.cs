a13163eb4051273a6962fe84d3b4290388b77e52
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 12:48:58 +1000

    setup audit interceptor in ctx

--- SpeedrunAuditingApi/Contexts/MyContext.cs
+++ SpeedrunAuditingApi/Contexts/MyContext.cs
@@ -7,18 +7,25 @@ public class MyContext : DbContext
 {
   public DbSet<CreditCard> CreditCards { get; set; }
   private readonly object _dbPath;
+  private readonly AuditInterceptor _auditInterceptor;
 
-  public MyContext(DbContextOptions<MyContext> opts) : base(opts)
+  public MyContext(
+    DbContextOptions<MyContext> opts,
+    AuditInterceptor auditInterceptor
+  ) : base(opts)
   {
     var rootPath = AppDomain.CurrentDomain.BaseDirectory;
     _dbPath = Path.Combine(rootPath, "MyContext.db");
+    _auditInterceptor = auditInterceptor;
   }
 
   protected override void OnConfiguring(DbContextOptionsBuilder opts)
   {
     if (opts.IsConfigured) return;
     var connString = $"Data Source={_dbPath}";
-    opts.UseSqlite(connString);
+    opts
+      .UseSqlite(connString)
+      .AddInterceptors(_auditInterceptor);
   }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
