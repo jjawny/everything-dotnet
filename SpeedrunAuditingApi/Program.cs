85a63474792cea65a43cb57efbf28e3e8479fa0a
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 12:26:06 +1000

    starter webapi

--- SpeedrunAuditingApi/Program.cs
+++ SpeedrunAuditingApi/Program.cs
@@ -1,44 +1,30 @@
+using SpeedrunAuditingApi.Contexts;
+
+// 1. Add services into DI container
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<MyContext>();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+// 2. Configure the HTTP request (middleware) pipeline.
+var app = builder.Build();
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseHttpsRedirection();
+}
 
-app.MapGet("/weatherforecast", () =>
+// 3. Endpoints
+app.MapGet("/api/placeholder", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return Results.NotFound();
+});
+
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
